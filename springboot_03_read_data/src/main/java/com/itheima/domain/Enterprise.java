c4526751408add1487a4b939b65d6b781ac706d0
郭雨 <1262425287@qq.com>
2024-07-21 00:30:22 +0800

    主要知识点在yaml文件中数据的读取

---
+++ springboot_03_read_data/src/main/java/com/itheima/domain/Enterprise.java
@@ -0,0 +1,68 @@
+package com.itheima.domain;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Component
+@ConfigurationProperties(prefix = "enterprise")
+public class Enterprise {
+
+    private String name;
+    private Integer age;
+    private String tel;
+    private String[] subject;
+
+    @Override
+    public String toString() {
+        return "Enterprise{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", tel='" + tel + '\'' +
+                ", subject=" + Arrays.toString(subject) +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String[] getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String[] subject) {
+        this.subject = subject;
+    }
+
+    public Enterprise(String name, Integer age, String tel, String[] subject) {
+        this.name = name;
+        this.age = age;
+        this.tel = tel;
+        this.subject = subject;
+    }
+
+    public Enterprise() {
+    }
+}
