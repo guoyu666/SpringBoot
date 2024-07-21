17f965ca36d71ce257e085fd43836329ea9ac2fd
郭雨 <1262425287@qq.com>
2024-07-21 16:15:25 +0800

    修改部分代码

---
+++ springboot_07_mybatis/src/main/java/com/itheima/dao/BookDao.java
@@ -0,0 +1,12 @@
+package com.itheima.dao;
+
+import com.itheima.domain.Book;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface BookDao {
+
+    @Select("select * from book where id = #{id}")
+    public Book getById(Integer id);
+}
