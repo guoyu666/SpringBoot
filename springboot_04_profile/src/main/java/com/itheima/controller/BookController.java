b004a87f11029208297f7e660ab7409d1e89d911
郭雨 <1262425287@qq.com>
2024-07-21 10:51:31 +0800

    提交部分代码

---
+++ springboot_04_profile/src/main/java/com/itheima/controller/BookController.java
@@ -0,0 +1,38 @@
+package com.itheima.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/books")
+public class BookController {
+
+    @Value("${lesson}")
+    private String lesson;
+
+    @Value("${server.port}")
+    private Integer port;
+
+    @Value("${enterprise.subject[0]}")
+    private String subject_00;
+
+    @Autowired
+    private Environment environment;
+
+    @GetMapping("/{id}")
+    public String getById(@PathVariable Integer id) {
+
+        System.out.println(lesson);
+        System.out.println(port);
+        System.out.println(subject_00);
+        System.out.println(environment.getProperty("server.port"));
+
+        System.out.println("获取图书id：" + id);
+        return "hello, Spring Boot";
+    }
+}
